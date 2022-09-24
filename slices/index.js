472f47ac5717a5293dce5f4e446d666d7542bdac
Octavio Amietta <octavioa1997@gmail.com>
2022-09-24 16:23:59 -0300

    UPD: Redux

--- slices/index.js
+++ slices/index.js
@@ -1,37 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    value: 0,
-    user: []
+    data: []
 }
 
-export const userDataSlice = createSlice({
-    name: 'userData',
+export const DataSlice = createSlice({
+    name: 'dataSlice',
     initialState,
     reducers: {
-        // increment: (state) => {
-        //     // Redux Toolkit allows us to write "mutating" logic in reducers. It
-        //     // doesn't actually mutate the state because it uses the Immer library,
-        //     // which detects changes to a "draft state" and produces a brand new
-        //     // immutable state based off those changes
-        //     state.value += 1
-        // },
-        // decrement: (state) => {
-        //     state.value -= 1
-        // },
-        // incrementByAmount: (state, action) => {
-        //     state.value += action.payload
-        // },
-        setUser: (state, action) => {
-            console.log("action: ", action);
-            state.user = action.payload;
+        setData: (state, action) => {
+            state.data = action.payload;
         }
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { setUser } = userDataSlice.actions
+export const { setData } = DataSlice.actions
 
-export const userValue = (state) => state.data.user;
+export const dataValue = (state) => state.dataReducer.data;
 
-export default userDataSlice.reducer
+export default DataSlice.reducer
--- store.js
+++ store.js
@@ -3,6 +3,6 @@ import dataReducer from './slices/index';
 
 export const store = configureStore({
     reducer: {
-        data: dataReducer,
+        dataReducer: dataReducer,
     },
 })
